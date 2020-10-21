d5de0ebcca0cd00bb53ccb7648b2f1efbe00b264
Nalee Star <ahn.jinhi@gmail.com>
2020-10-20 20:43:49 -0400

    added connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,8 +15,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useUnifiedTopology: true,
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
